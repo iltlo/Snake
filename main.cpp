9b963cd4723da28c757472e59f2889b20a92deb4
Horace <laipokhang08@gmail.com>
2022-03-07 12:31:00 +0800

    optimizing + commenting start menu code, added refreshing feature

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "start_menu.hpp"
 #include "new_game.hpp"
 
+// main loop of the game,
+// exit only when game closed
 int main()
 {
     initscr();
@@ -12,16 +14,21 @@ int main()
 
     bool n_exit_game = true;
 
+    // branching the game into serval modes (features/mode to be added)
     while(n_exit_game)
     {
-        switch(start_menu())
+        WINDOW* menu_pic_win = newwin(menu_pic_y, menu_pic_x, 0, 0);
+        WINDOW* option_win = newwin(option_win_y, option_win_x, 0, menu_pic_x);
+
+        switch(start_menu(menu_pic_win, option_win))
         {
             case(s_new_game):
             new_game();
             break;
 
             // case(s_option):
-            // option();
+            //     option();
+            //     break;
 
             case(s_exit_game):
                 n_exit_game = false;
@@ -29,7 +36,7 @@ int main()
                 break;
 
             default:
-                break;
+                continue;
         }
     }
     endwin();
